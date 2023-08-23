39b0dad98535060c20aa2a5fda6ae5c226224498
lucasCardozo <lucasivancardozo27@gmial.com>
2023-08-23 16:54:35 -0300

    inicio de app con gatsby

---
+++ src/pages/404.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <main>
+      <h1>este es el error 404</h1>
+    </main>
+  );
+};
+
+export default NotFoundPage;
+
+export const Head = () => <title>Not found</title>;
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Hola como andas es la primer pagina</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
